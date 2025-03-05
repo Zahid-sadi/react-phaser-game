9e48dbc6c8285cd3c3016786ec36c334e6eb85ed
zahid-sadi <zahidbinkashem@gmail.com>
2025-03-06 00:20:29 +0600

    added button config to style start buton

---
+++ src/button-config.ts
@@ -0,0 +1,16 @@
+export const buttonConfig = {
+    buttonText: 'Start Game',
+    buttonStyle: {
+      color: '#FFFFFF',
+      backgroundColor: '#A953FF',
+      top: '75%',
+      left: '50%',
+      width: '70%',
+      height: '48px',
+      borderRadius: '8px',
+      fontSize: '24px',
+      transform: 'translateX(-50%)', 
+      position: 'absolute', 
+    },
+  };
+  
